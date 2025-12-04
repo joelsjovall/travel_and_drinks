674f1be140f18ce095d8b09fbe6188d94d364bb6
Sigge <siggeelli@gmail.com>
2025-12-04 14:07:39 +0100

    lägger till text från nodehill

---
+++ program.cs
@@ -0,0 +1,52 @@
+using MySql.Data.MySqlClient;
+using server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Config config = new("server=127.0.0.1;uid=root;pwd=rootroot;database=test");
+builder.Services.AddSingleton<Config>(config);
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+app.UseSession();
+
+
+app.MapGet("/login", Login.Get);
+app.MapPost("/login", Login.Post);
+app.MapDelete("/login", Login.Delete);
+
+app.MapGet("/users", Users.GetAll);
+app.MapPost("/users", Users.Post);
+app.MapGet("/users/{id}", Users.Get);
+app.MapDelete("/users/{id}", Users.Delete);
+
+// special, reset db
+app.MapDelete("/db", db_reset_to_default);
+
+app.Run();
+
+
+
+
+async Task db_reset_to_default(Config config)
+{
+
+    // Drop all tables from database
+    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, "DROP TABLE IF EXISTS users");
+
+    // Create all tables
+    string users_table = """
+        CREATE TABLE users
+        (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            email varchar(256) NOT NULL UNIQUE,
+            password TEXT
+        )
+    """;
+    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, users_table);
+}
