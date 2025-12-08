bd9b11ef8a49bf5b1c48b4286a74c26f37562cd3
Sigge <siggeelli@gmail.com>
2025-12-08 14:16:46 +0100

    idk

--- Login.cs
+++ Login.cs
@@ -1,41 +1,5 @@
-<<<<<<< HEAD
-global using MySql.Data.MySqlClient;
-using server;
 
 
-Config config = new("server=127.0.0.1;uid=root;pwd=kebab123;database=drinks_and_travel");
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton(config);
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.HttpOnly = true;
-   options.Cookie.IsEssential = true;
-});
-
-var app = builder.Build();
-app.UseSession();
-app.MapGet("/", () => "Hello world");
-
-app.Run();
-
-async Task db_reset_to_default()
-{
-    string create_users = """
-    Create Table users
-    (
-    id INT PRIMARY KEY AUTO_INCERMENT,
-    email VARCHAR(254) NOT NULL UNIQUE,
-    password VARCHAR(128)
-    
-    )
-""";
-await MySqlHelper.ExecuteNonQueryAsync(config.db, "DROP TABEL IF EXISTS users"):
-await MySqlHelper.ExecuteNonQueryAsync(config.db, create_users)
-}
-
-
-=======
 namespace server;
 
 static class Login
@@ -72,4 +36,3 @@ static class Login
         return result;
     }
 }
->>>>>>> 7b22cea96aa087b9fda96eceabafc015f1ef541a
--- program.cs
+++ program.cs
@@ -1,67 +1,3 @@
-/* global using MySql.Data.MySqlClient;
-using server;
-
-var builder = WebApplication.CreateBuilder(args);
-
-Config config = new ("server=127.0.0.1;uid=root;pwd=kebab123;database=drinks_and_travel");
-builder.Services.AddSingleton<Config>(config);
-/*
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-*/
-var app = builder.Build();
-//app.UseSession();
-
-
-//app.MapGet("/login", Login.Get);
-//app.MapPost("/login", Login.Post);
-//app.MapDelete("/login", Login.Delete);
-
-app.MapGet("/users", Users.GetAll);
-app.MapPost("/users", Users.Post);
-app.MapGet("/users/{id}", Users.Get);
-app.MapDelete("/users/{id}", Users.Delete);
-
-// special, reset db
-app.MapDelete("/db", db_reset_to_default);
-
-app.Run();
-
-
-
-
-async Task db_reset_to_default(Config config)
-{
-
-    // Drop all tables from database
-    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, "DROP TABLE IF EXISTS users");
-
-    // Create all tables
-    string users_table = """
-        CREATE TABLE users
-        (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            email varchar(256) NOT NULL UNIQUE,
-            password TEXT
-        )
-    """;
-    await MySqlHelper.ExecuteNonQueryAsync(config.ConnectionString, users_table);
-}
-
-<<<<<<< HEAD
-=======
-*/
-
-
-
-
-
-
-
 global using MySql.Data.MySqlClient;
 using System.ComponentModel;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -104,4 +40,3 @@ async Task db_reset_to_defaut(Config config)
 }
 
 
->>>>>>> 7b22cea96aa087b9fda96eceabafc015f1ef541a
