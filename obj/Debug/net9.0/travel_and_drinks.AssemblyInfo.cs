6869c53ffd4546f1dd2e0e9012ca8d4ff10ad7c9
mansoskarsson <mansoskarssonrhotmail.com>
2025-12-09 10:50:28 +0100

    nyast

--- obj/Debug/net9.0/travel_and_drinks.AssemblyInfo.cs
+++ obj/Debug/net9.0/travel_and_drinks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("travel_and_drinks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbd93bda5a640037809495002b75b71daf323f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a759184740b86f8c71f051bad65bfe493e268b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("travel_and_drinks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("travel_and_drinks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
